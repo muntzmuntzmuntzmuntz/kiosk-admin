69bdac64dd52bc4b02bda256d91eac11e9c7bca4
Felix <iron59.evangelista@gmail.com>
2026-05-04 01:05:16 +0800

    add cloudflare

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,8 @@
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@/app/generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
+import { cache } from "react";
 
-const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
-
-function createPrismaClient() {
+export const getDb = cache(() => {
   const connectionString = process.env.DATABASE_URL;
 
   if (!connectionString) {
@@ -11,12 +10,12 @@ function createPrismaClient() {
   }
 
   return new PrismaClient({
-    adapter: new PrismaPg({ connectionString }),
+    adapter: new PrismaPg({ connectionString, maxUses: 1 }),
   });
-}
+});
 
-export const prisma =
-  globalForPrisma.prisma ||
-  createPrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+export const prisma = new Proxy({} as PrismaClient, {
+  get(_target, property) {
+    return getDb()[property as keyof PrismaClient];
+  },
+});
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: [
+    "@prisma/adapter-pg",
+    "@prisma/client",
+    ".prisma/client",
+    "pg",
+    "pg-cloudflare",
+  ],
+  outputFileTracingIncludes: {
+    "/*": [
+      "./node_modules/pg-cloudflare/dist/**/*",
+      "./node_modules/pg-cloudflare/esm/**/*",
+    ],
+  },
 };
+
+initOpenNextCloudflareForDev();
 
 export default nextConfig;
